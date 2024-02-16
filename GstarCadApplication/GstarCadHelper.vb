c4bff008e570e12a89a6bf1a26fad24825b210c1
HemanthAppikatla <Hemanth.appikatla@gmail.com>
2024-02-16 12:33:40 +0530

    Gstar Cad Application to perform: 1.Load.                                   2.Disable/Enable layers.                                   3.Save.

---
+++ GstarCadApplication/GstarCadHelper.vb
@@ -0,0 +1,101 @@
+﻿Imports GrxCAD.Interop
+
+Public Class GstarCadHelper
+    Public Property CadApplication As GcadApplication
+    Public CadDocument As GcadDocument = Nothing
+
+    ''' <summary>
+    ''' Connects to Gstar Cad application
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function LaunchGstarCad() As Boolean
+        Try
+            ' Starting Gstar Cad Application
+            CadApplication = New GcadApplication()
+            CadApplication.Visible = True
+
+            Return CadApplication IsNot Nothing
+        Catch ex As Exception
+        End Try
+
+        Return False
+    End Function
+
+    ''' <summary>
+    ''' Load/Open model in GstarCad Environment
+    ''' </summary>
+    ''' <param name="modelPath">DXF/DWG file path to open</param>
+    ''' <returns>True - Success, False - Failure</returns>
+    Public Function Open(modelPath As String) As Boolean
+        If CadApplication Is Nothing Then
+            Return False
+        End If
+
+        Dim gCadDocs As GcadDocuments = CadApplication.Documents
+        CadDocument = gCadDocs.Open(modelPath)
+
+        Return CadDocument IsNot Nothing
+    End Function
+
+    ''' <summary>
+    ''' Gets all the Layers information of current document
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function GetAllLayers() As List(Of String)
+        Dim layerNames As New List(Of String)()
+        If CadDocument Is Nothing Then
+            Return layerNames
+        End If
+        ''get all the layers available in the current Document
+        Dim gCadLayers As GcadLayers = CadDocument.Layers
+        ''layers count
+        Dim layerCount As Integer = gCadLayers.Count
+
+        For idx As Integer = 0 To layerCount - 1
+            Dim gCadLayer As GcadLayer = gCadLayers.Item(idx)
+            Dim layerName As String = gCadLayer.Name
+            layerNames.Add(layerName)
+        Next
+        ' Add code to retrieve layer names from GstarCAD
+
+        Return layerNames
+    End Function
+
+    ''' <summary>
+    ''' Enables/Disables the respected Layers based on User selection
+    ''' </summary>
+    ''' <param name="LayerNames">All the Layers Names To Update</param>
+    ''' <param name="enable">Status to enable or disable
+    '''                      True --> Enables the Layers
+    '''                      False--> Disables the Layers</param>
+    ''' <returns></returns>
+    Public Function EnableOrDisableLayers(LayerNames As List(Of String), enable As Boolean) As Boolean
+        'get the existing layers
+        Dim gCadLayers As GcadLayers = CadDocument.Layers
+        Dim layerCount As Integer = gCadLayers.Count
+
+        For idx As Integer = 0 To layerCount - 1
+            Dim gCadLayer As GcadLayer = gCadLayers.Item(idx)
+            Dim layerName As String = gCadLayer.Name
+            'check if we need to update the status of this layer
+            If LayerNames.Contains(layerName) Then
+                gCadLayer.LayerOn = enable
+            End If
+        Next
+
+        CadDocument.Regen(GcRegenType.acAllViewports) 'regen after chnages
+        Return True
+    End Function
+
+
+    ''' <summary>
+    ''' Save active  CAD document with latest changes
+    ''' </summary>
+    Public Sub SaveDoc()
+        If CadDocument IsNot Nothing Then
+            CadDocument.Save()
+        End If
+    End Sub
+
+
+End Class
---
+++ GstarCadApplication/MainWindow.xaml.vb
@@ -0,0 +1,111 @@
+﻿Imports Microsoft.Win32
+
+Class MainWindow
+
+    Dim cadHelper As GstarCadHelper
+    Public Sub New()
+        InitializeComponent()
+        cadHelper = New GstarCadHelper()
+    End Sub
+
+    ''' <summary>
+    ''' Loads the respective cad file from local directory into Cad Environment
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button_LoadOnClick(sender As Object, e As RoutedEventArgs)
+        Dim fileDialog As New OpenFileDialog With {
+            .Title = "Please select G star Cad file",
+            .Filter = "DWG|*.dwg"
+        }
+        Dim dialogResult As Boolean? = fileDialog.ShowDialog()
+        If dialogResult = True Then
+            Dim selectedFile As String = fileDialog.FileName
+            If Not String.IsNullOrEmpty(selectedFile) Then
+                TxtBox_InputFile.Text = selectedFile
+            End If
+        Else
+            MessageBox.Show("Cancelled")
+        End If
+    End Sub
+
+    Private Sub ButtonShowAllLayers_OnClick(sender As Object, e As RoutedEventArgs)
+        'get all the layers info of the Document
+        Dim allLayers As List(Of String) = cadHelper.GetAllLayers()
+        'display the layers info 
+        LayersInfo.ItemsSource = allLayers
+    End Sub
+
+    Private Sub ButtonEnableLayers_OnClick(sender As Object, e As RoutedEventArgs)
+        If LayersInfo.SelectedItems.Count > 0 Then
+            Dim selectedLayers As List(Of String) = New List(Of String) ' = CType(LayersInfo.SelectedItems, List(Of String))
+            For Each item As Object In LayersInfo.SelectedItems
+                Dim itemName As String = item.ToString
+                selectedLayers.Add(item)
+            Next
+
+            cadHelper.EnableOrDisableLayers(selectedLayers, True)
+            MessageBox.Show("Enabled the selected Layers")
+        End If
+        'reset the UI settings
+        'CheckBox_SelectAllLayers.IsChecked = False
+    End Sub
+
+    Private Sub ButtonDisableLayers_OnClick(sender As Object, e As RoutedEventArgs)
+        If LayersInfo.SelectedItems.Count > 0 Then
+            Dim selectedLayers As List(Of String) = New List(Of String)
+            For Each item As Object In LayersInfo.SelectedItems
+                Dim itemName As String = item.ToString
+                selectedLayers.Add(item)
+            Next
+            cadHelper.EnableOrDisableLayers(selectedLayers, False)
+            MessageBox.Show("Disabled Selected Layers")
+        End If
+        'reset the UI settings
+        'CheckBox_SelectAllLayers.IsChecked = False
+    End Sub
+
+    'Performs save operation  
+    Private Sub ButtonSave_OnClick(sender As Object, e As RoutedEventArgs)
+        cadHelper.SaveDoc()
+    End Sub
+
+    Private Sub CheckBox_SelectAllLayers_Click(sender As Object, e As RoutedEventArgs)
+        If (CheckBox_SelectAllLayers.IsChecked) Then
+            LayersInfo.SelectAll()
+        Else
+            LayersInfo.SelectedItems.Clear()
+        End If
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Opens the selected file in Respective Cad application
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button_Open_Click(sender As Object, e As RoutedEventArgs)
+        Dim filePath = TxtBox_InputFile.Text
+        If Not String.IsNullOrEmpty(filePath) Then
+            Dim loadResult As Boolean = cadHelper.Open(filePath)
+            If loadResult Then
+                MessageBox.Show("Loaded the Selected file in Cad")
+            End If
+        End If
+    End Sub
+
+    Private Sub ApplicationUi_OnLoad(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Launchs Cad Application
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button_LaunchCad(sender As Object, e As RoutedEventArgs)
+        cadHelper.LaunchGstarCad()
+    End Sub
+End Class
